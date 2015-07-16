5526ff3605bcfb796b3f23abfce452f8cf2e61d2
Andi Anton <andianton@Mihail-Florins-Mac-mini.local>
2015-07-16 12:07:36 +0300

    Initial Commit

---
+++ collectionViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  collectionViewExample
+//
+//  Created by Andi Anton on 16/07/15.
+//  Copyright (c) 2015 Andi Anton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ collectionViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionViewExample
+//
+//  Created by Andi Anton on 16/07/15.
+//  Copyright (c) 2015 Andi Anton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
