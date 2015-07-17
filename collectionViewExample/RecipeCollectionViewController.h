7abe00080236b64953d64c5978e0140c10c5a42c
Andi Anton <andianton@Mihail-Florins-Mac-mini.local>
2015-07-17 10:54:38 +0300

    first commit

---
+++ collectionViewExample/RecipeCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeCollectionViewController.h
+//  collectionViewExample
+//
+//  Created by Andi Anton on 16/07/15.
+//  Copyright (c) 2015 Andi Anton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeCollectionViewController : UICollectionViewController
+
+@end
